17ce980699ba893c986687f273d60a18a738935b
Volodymyr Husak <volodyaog2907@gmail.com>
2022-09-28 20:11:09 +0300

    corrects component FriendList

--- src/components/FriendList/Friend.jsx
+++ src/components/FriendList/Friend.jsx
@@ -1,11 +1,31 @@
 import css from './FriendList.module.css'
+import propTypes from "prop-types";
 
 export const Friend = ({friends}) => {
     return (friends.map(({id, isOnline, name, avatar}) => 
-        <li className={css.item} id ={id}>
-            <span className={css.status}>{isOnline}</span>
-                <img className={css.avatar} src={avatar} alt="User avatar" width="200" />
+        <li className={css.item} key={id}>
+            <span className={css.status} style={{ backgroundColor: getBackgroundColor(isOnline) }}></span>
+            <img className={css.avatar} src={avatar} alt="User avatar" width="200" />
             <p className={css.name}>{name}</p>
         </li>)
     )
 }
+
+function getBackgroundColor(bool) {
+    if (bool) {
+        return 'green';
+    }
+    return 'tomato';
+}
+
+Friend.propTypes = {
+        friends: propTypes.arrayOf(
+        propTypes.shape({
+            avatar: propTypes.string.isRequired,
+            name: propTypes.string.isRequired,
+            isOnline: propTypes.bool.isRequired,
+            id: propTypes.number
+        }
+        ),
+    ),
+};
