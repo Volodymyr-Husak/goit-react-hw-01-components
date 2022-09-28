841787dfaff64b8d3d33dc00c9a5b5157f491614
Volodymyr Husak <volodyaog2907@gmail.com>
2022-09-28 12:12:42 +0300

    add styles to component Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,16 @@
-// import css from './Statistics.module.css'
+import css from './Statistics.module.css'
 // import PropTypes from "prop-types";
 
 export const Statistics = ({title, stats}) => {
     
     return (
-        <section class="statistics">
-            <h2 class="title">{title}</h2>
+        <section className={css.statistics}>
+            <h2 className={css.title}>{title}</h2>
 
-            <ul class="stat-list">{stats.map(stat => (
-                    <li class="item">
-                        <span class="label">{stat.label}</span>
-                        <span class="percentage">{stat.percentage}</span>
+            <ul className={css.stat_list}>{stats.map(stat => (
+                <li className={css.item} key={stat.id} style={{ backgroundColor: getRandomHexColor() }}>
+                        <span className="label">{stat.label}</span><br/>
+                        <span className="percentage">{stat.percentage}%</span>
                     </li>
                 ))}        
             </ul>
@@ -19,10 +19,38 @@ export const Statistics = ({title, stats}) => {
     )
 }
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// const itemEl = document.querySelector({css.item});
+
+
+
+// console.log(itemEl)
+
 // Statistics.PropTypes = {
-//     title: PropTypes.string, 
-//     stats: PropTypes.arrayOf(optionalObjectWithStrictShape : PropTypes . exact ( { 
-//     optionalProperty : PropTypes . string , 
-//     requiredProperty : PropTypes . number . isRequired 
-//   } ) )
-// }
+//     title: PropTypes.string.isRequired,
+//     stats: PropTypes.arrayOf(
+//         PropTypes.objectOf({
+//             id: PropTypes.string.isRequired,
+//             label: PropTypes.string.isRequired,
+//             percentage: PropTypes.number.isRequired
+//         }),
+//     ),
+// };
+
+
+// Profile.propTypes = {
+//     props: PropTypes.exact ( { 
+//             username: PropTypes.string.isRequired,
+//             tag: PropTypes.string.isRequired, 
+//             location: PropTypes.string.isRequired, avatar: PropTypes.string.isRequired,
+//             stats: PropTypes.objectOf({
+//                 followers: PropTypes.number,
+//                 views: PropTypes.number,
+//                 likes: PropTypes.number,
+//         }),
+//     } )
+    
+// };
