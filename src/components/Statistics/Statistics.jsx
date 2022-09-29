d34523b7c53bb14ead4dece44ff05c5ddc8e163f
Volodymyr Husak <volodyaog2907@gmail.com>
2022-09-29 22:43:06 +0300

    corrects component Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,8 +5,8 @@ export const Statistics = ({title, stats}) => {
     
     return (
         <section className={css.statistics}>
-            <h2 className={css.title}>{title}</h2>
-
+            {title && <h2 className={css.title}>{title}</h2>}
+            
             <ul className={css.stat_list}>{stats.map(stat => (
                 <li className={css.item} key={stat.id} style={{ backgroundColor: getRandomHexColor() }}>
                         <span className="label">{stat.label}</span><br/>
@@ -25,7 +25,7 @@ function getRandomHexColor() {
 
 
 Statistics.propTypes = {
-    title: propTypes.string.isRequired,
+    title: propTypes.string,
     stats: propTypes.arrayOf(
         propTypes.shape({
             id: propTypes.string.isRequired,
